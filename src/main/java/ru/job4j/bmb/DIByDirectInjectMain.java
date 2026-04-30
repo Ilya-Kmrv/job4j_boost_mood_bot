655053c07bd51374b1fe870e8b2aaf4bcfd91ab8
srjm <cyberpiluha@gmail.com>
2026-04-30 11:52:05 +0300

    Внедрение коллекций | Поставщики контекста [#505300]

--- src/main/java/ru/job4j/bmb/DIByDirectInjectMain.java
+++ src/main/java/ru/job4j/bmb/DIByDirectInjectMain.java
@@ -8,6 +8,7 @@ public class DIByDirectInjectMain {
     public static void main(String[] args) {
         var handler = new BotCommandHandler();
         var tg = new TelegramBotService(handler);
-        tg.receive(new Content());
+        Long chatId = 1L;
+        tg.receive(new Content(chatId));
     }
 }
