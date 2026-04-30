ef8cb716515d62e3510e702237a6cf1361fe1900
srjm <cyberpiluha@gmail.com>
2026-04-30 14:32:00 +0300

    Механизм рассылки уведомлений [#505297]

--- src/main/java/ru/job4j/bmb/services/TgRemoteService.java
+++ src/main/java/ru/job4j/bmb/services/TgRemoteService.java
@@ -8,7 +8,8 @@ import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.meta.api.objects.replykeyboard.InlineKeyboardMarkup;
 import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.InlineKeyboardButton;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
-
+import ru.job4j.bmb.repository.UserRepository;
+import ru.job4j.bmb.model.User;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -19,7 +20,7 @@ public class TgRemoteService extends TelegramLongPollingBot {
 
     private final String botName;
     private final String botToken;
-
+    private final UserRepository userRepository;
     private static final Map<String, String> MOOD_RESP = new HashMap<>();
 
     static {
@@ -31,9 +32,11 @@ public class TgRemoteService extends TelegramLongPollingBot {
     }
 
     public TgRemoteService(@Value("${telegram.bot.name}") String botName,
-                           @Value("${telegram.bot.token}") String botToken) {
+                           @Value("${telegram.bot.token}") String botToken,
+                           UserRepository userRepository) {
         this.botName = botName;
         this.botToken = botToken;
+        this.userRepository = userRepository;
     }
 
     @Override
@@ -46,7 +49,7 @@ public class TgRemoteService extends TelegramLongPollingBot {
         return botName;
     }
 
-    private void send(SendMessage message) {
+    public void send(SendMessage message) {
         try {
             execute(message);
         } catch (TelegramApiException e) {
@@ -83,14 +86,16 @@ public class TgRemoteService extends TelegramLongPollingBot {
 
     @Override
     public void onUpdateReceived(Update update) {
-        if (update.hasCallbackQuery()) {
-            var data = update.getCallbackQuery().getData();
-            var chatId = update.getCallbackQuery().getMessage().getChatId();
-            send(new SendMessage(String.valueOf(chatId), MOOD_RESP.get(data)));
-        }
         if (update.hasMessage() && update.getMessage().hasText()) {
-            long chatId = update.getMessage().getChatId();
-            send(sendButtons(chatId));
+            var message = update.getMessage();
+            if ("/start".equals(message.getText())) {
+                long chatId = message.getChatId();
+                var user = new User();
+                user.setClientId(message.getFrom().getId());
+                user.setChatId(chatId);
+                userRepository.add(user);
+                send(sendButtons(chatId));  // Метод для отправки кнопок пользователю
+            }
         }
     }
 }
