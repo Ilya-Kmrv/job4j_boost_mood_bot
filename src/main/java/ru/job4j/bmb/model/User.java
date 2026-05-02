e46848a852e61af0cccd45ae9c512b3828de15b9
srjm <cyberpiluha@gmail.com>
2026-05-02 19:55:32 +0300

    Spring Data JPA [#505298]

--- src/main/java/ru/job4j/bmb/model/User.java
+++ src/main/java/ru/job4j/bmb/model/User.java
@@ -1,11 +1,20 @@
 package ru.job4j.bmb.model;
 
+import jakarta.persistence.*;
+
 import java.util.Objects;
 
+@Entity
+@Table(name = "mb_user")
 public class User {
-
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(name = "client_id")
     private long clientId;
+
+    @Column(name = "chat_id")
     private long chatId;
 
     public User() {
@@ -46,10 +55,11 @@ public class User {
         if (this == o) {
             return true;
         }
-        if (!(o instanceof User user)) {
+        if (o == null || getClass() != o.getClass()) {
             return false;
         }
-        return clientId == user.clientId && chatId == user.chatId && Objects.equals(id, user.id);
+        User user = (User) o;
+        return Objects.equals(id, user.id);
     }
 
     @Override
