c3aa2c084ad0f85737a543bfa2a789fd47a24b6e
srjm <cyberpiluha@gmail.com>
2026-05-04 14:41:25 +0300

    SentContent - интеграция Content с Telegram API [#505307]

--- src/main/java/ru/job4j/bmb/DIByDirectInjectMain.java
+++ src/main/java/ru/job4j/bmb/DIByDirectInjectMain.java
@@ -7,7 +7,7 @@ import ru.job4j.bmb.services.TelegramBotService;
 public class DIByDirectInjectMain {
     public static void main(String[] args) {
         var handler = new BotCommandHandler(null, null, null);
-        var tg = new TelegramBotService(handler);
+        var tg = new TelegramBotService("test", "123", handler);
         Long chatId = 1L;
         //tg.receive(new Content(chatId));
     }
--- src/main/java/ru/job4j/bmb/services/TelegramBotService.java
+++ src/main/java/ru/job4j/bmb/services/TelegramBotService.java
@@ -3,36 +3,82 @@ package ru.job4j.bmb.services;
 import jakarta.annotation.PostConstruct;
 import jakarta.annotation.PreDestroy;
 import org.springframework.beans.factory.BeanNameAware;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.send.SendAudio;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.methods.send.SendPhoto;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 import ru.job4j.bmb.content.Content;
 
 @Service
-public class TelegramBotService implements BeanNameAware {
-
+public class TelegramBotService extends TelegramLongPollingBot implements SentContent {
     private final BotCommandHandler handler;
-    private String beanName;
+    private final String botName;
 
-    public TelegramBotService(BotCommandHandler handler) {
+    public TelegramBotService(@Value("${telegram.bot.name}") String botName,
+                              @Value("${telegram.bot.token}") String botToken,
+                              BotCommandHandler handler) {
+        super(botToken);
         this.handler = handler;
+        this.botName = botName;
     }
-
-    /*public void receive(Content content) {
-        handler.receive(content);
-    }*/
 
     @Override
-    public void setBeanName(String name) {
-        this.beanName = name;
-        System.out.println("Имя бина: " + name);
+    public void onUpdateReceived(Update update) {
+        if (update.hasCallbackQuery()) {
+            handler.handleCallback(update.getCallbackQuery())
+                    .ifPresent(this::sent);
+        } else if (update.hasMessage() && update.getMessage().getText() != null) {
+            handler.commands(update.getMessage())
+                    .ifPresent(this::sent);
+        }
     }
 
-    @PostConstruct
-    public void init() {
-        System.out.println("Bean is going through @PostConstruct init.");
+    @Override
+    public String getBotUsername() {
+        return botName;
     }
 
-    @PreDestroy
-    public void destroy() {
-        System.out.println("Bean will be destroyed via @PreDestroy.");
+    @Override
+    public void sent(Content content) {
+        try {
+            if (content.getAudio() != null) {
+                var audio = new SendAudio();
+                audio.setChatId(content.getChatId());
+                audio.setAudio(content.getAudio());
+                if (content.getText() != null) {
+                    audio.setCaption(content.getText());
+                }
+                execute(audio);
+
+            } else if (content.getMarkup() != null) {
+                var message = new SendMessage();
+                message.setChatId(content.getChatId());
+                message.setText(content.getText());
+                message.setReplyMarkup(content.getMarkup());
+                execute(message);
+
+            } else if (content.getPhoto() != null) {
+                var photo = new SendPhoto();
+                photo.setChatId(content.getChatId());
+                photo.setPhoto(content.getPhoto());
+                if (content.getText() != null) {
+                    photo.setCaption(content.getText());
+                }
+                execute(photo);
+
+            } else if (content.getText() != null) {
+                var message = new SendMessage();
+                message.setChatId(content.getChatId());
+                message.setText(content.getText());
+                execute(message);
+            }
+
+        } catch (TelegramApiException e) {
+            throw new SentContentException("Ошибка отправки контента", e);
+        }
     }
 }
